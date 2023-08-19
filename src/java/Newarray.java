c9aa3175ceb914cca1da0a69b3e0299304599a50
samirchandekar <samirchandekar527@gmail.com>
2023-08-19 12:26:42 +0530

    Array program

---
+++ src/java/Newarray.java
@@ -0,0 +1,11 @@
+public class Newarray{
+public static void main(String[] arg){
+
+int number[] ={23,49,46,38};
+System.out.println(number[3]);
+
+for(int i = 0; i<=number.length; i++){
+System.out.println(number[i]);
+}
+}
+}
